dc98370f720088e808e68926f5efab56592e51be
Marco <marcosilhaag31@gmail.com>
2026-07-26 17:40:53 -0300

    :sparkles: breadcrumb dinamico, como prometido

---
+++ src/app/features/modules/platform/tenant/tenant.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-platform-tenant',
+  standalone: true,
+  template: `<h1>Clientes</h1>`
+})
+export class PlatformTenantComponent{ }
